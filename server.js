07163ebdfc7f40c713d7a468461f069e54a59763
coderay <dev@coderay.online>
2024-03-04 04:07:56 +0100

    new 404 server

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 3500
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root.js'))
+app.all('*', (req, res) =>{
+    res.status(404)
+    if (req.accepts('html')){
+        res.sendFile(Path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')){
+        res.json({message: '404 not found'})
+    } else(res.type('txt').send('404 not found'))
+})
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
 
 
